baa8186d52bdf0ca4600550d35566ec260a3403b
lwande <lwandedana01@gmail.com>
2026-07-04 22:26:09 +0200

    week 1 wdd131 completed

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+// Current Year
+
+const year = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+// Last Modified
+
+document.getElementById("lastModified").textContent =
+"Last Modified: " + document.lastModified;
