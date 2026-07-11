79ad1a911472d1e91205e7d581918473febcd18b
lwande <lwandedana01@gmail.com>
2026-07-11 23:29:12 +0200

    week2 project completed

---
+++ styles/temples.js
@@ -0,0 +1,15 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+
+});
+
+document.querySelector("#year").textContent =
+new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+`Last Modified: ${document.lastModified}`;
